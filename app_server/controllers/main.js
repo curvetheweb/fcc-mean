4ded1eb69543da010605b684f7f68d0dab72b5fa
Michael Scola <mikescola@gmail.com>
2016-08-04 19:40:59 -0500

    Added twitter methods

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,4 +1,19 @@
 /* GET home page */
 module.exports.index = function (req, res) {
-	res.render('index', {title: 'Home'});
+	
+	var Twitter = require('twitter');
+
+	var twitterClient = new Twitter({
+	  consumer_key: '4hXf3mFnEoIcSW50gxnkGi9mX',
+	  consumer_secret: 'sts8ntmXaqSoc42vslPyLO0Exr0TINu35xN7tjtOTQfU42tTTj',
+	  access_token_key: '2461606823-KkPA3Xa6PxVKPENXOJx0EpvYLdZUZ15wfQlxZ8e',
+	  access_token_secret: 'eujEwY3gn2HxPHJQX02KCi4vAdHFPWPp6v5eNOjF4eTsh'
+	});
+	
+	var receivedTweets = twitterClient.get('statuses/user_timeline', {screen_name: 'freshcoastcap', count: 2, 'include_rts': false, 'exclude_replies': true}, function(error, tweets, response) {
+	    
+	    res.render('index', {title: 'Home', current_tweets: tweets});
+	    
+	});
+
 }
