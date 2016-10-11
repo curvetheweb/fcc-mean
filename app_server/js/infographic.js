c5033cff1e1896f39adddcff273da800d0e575ed
ctw-twilliams <twilliams@curvetheweb.com>
2016-10-10 19:23:57 -0500

    infographic temp

--- app_server/js/infographic.js
+++ app_server/js/infographic.js
@@ -1,35 +1,66 @@
 $(function(){
 	//Vacant Lot Chart
 	console.log(firing);
-	var ctx = $("#js-vacant-land-chart");
+	Chart.defaults.global.defaultFontColor = '#fff';
+//	Chart.defaults.global.maintainAspectRatio = false;
+	
+	var ctx = $("#js-vacant-land-chart"),
+		ctx2 = $('#js-tree-plantings-chart');
 	
 	var vacantLot = new Chart(ctx, {
 		type: 'bar',
 		data: {
-			labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+			labels: [['No New', 'Trees'], ['New Trees', 'Within 50ft.']],
 			datasets: [{
-				label: '# of Votes',
-				data: [12, 19, 3, 5, 2, 3],
+				label: 'House Price (Thousands)',
+				data: [35, 38.4],
 				backgroundColor: [
-					'rgba(255, 99, 132, 0.2)',
-					'rgba(54, 162, 235, 0.2)',
-					'rgba(255, 206, 86, 0.2)',
-					'rgba(75, 192, 192, 0.2)',
-					'rgba(153, 102, 255, 0.2)',
-					'rgba(255, 159, 64, 0.2)'
+					'rgba(255, 255, 255, 1)',
+					'rgba(255, 255, 255, 1)'
 				],
 				borderColor: [
-					'rgba(255,99,132,1)',
-					'rgba(54, 162, 235, 1)',
-					'rgba(255, 206, 86, 1)',
-					'rgba(75, 192, 192, 1)',
-					'rgba(153, 102, 255, 1)',
-					'rgba(255, 159, 64, 1)'
+					'rgba(255, 255, 255, 1)',
+					'rgba(255, 255, 255, 1)'
 				],
 				borderWidth: 1
 			}]
 		},
 		options: {
+			responsive: true,
+			maintainAspectRatio: false,
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero: false,
+						min: 30,
+						max: 40
+					}
+				}]
+			}
+		}
+	});
+	
+	var treePlantings = new Chart(ctx2, {
+		type: 'bar',
+		data: {
+			labels: [['No New', 'Trees'], ['New Trees', 'Within 50ft.']],
+			datasets: [{
+				label: 'Change to Avg. House',
+				data: [0, 3400],
+				backgroundColor: [
+					'rgba(255, 255, 255, 1)',
+					'rgba(255, 255, 255, 1)'
+				],
+				borderColor: [
+					'rgba(255, 255, 255, 1)',
+					'rgba(255, 255, 255, 1)'
+				],
+				borderWidth: 1
+			}]
+		},
+		options: {
+			responsive: true,
+			maintainAspectRatio: false,
 			scales: {
 				yAxes: [{
 					ticks: {
@@ -38,5 +69,10 @@ $(function(){
 				}]
 			}
 		}
+		
 	});
+	
+	
+	
+	
 });
