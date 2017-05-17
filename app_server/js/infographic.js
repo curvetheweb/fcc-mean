6d52ce6353652fd45d782bb8130126ad3f8fbcdf
twilliams540 <twilliams@curvetheweb.com>
2017-05-17 18:12:50 -0500

    Overhaul

--- app_server/js/infographic.js
+++ app_server/js/infographic.js
@@ -1,78 +1,78 @@
-$(function(){
-	//Vacant Lot Chart
-	console.log(firing);
-	Chart.defaults.global.defaultFontColor = '#fff';
-//	Chart.defaults.global.maintainAspectRatio = false;
-	
-	var ctx = $("#js-vacant-land-chart"),
-		ctx2 = $('#js-tree-plantings-chart');
-	
-	var vacantLot = new Chart(ctx, {
-		type: 'bar',
-		data: {
-			labels: [['No New', 'Trees'], ['New Trees', 'Within 50ft.']],
-			datasets: [{
-				label: 'House Price (Thousands)',
-				data: [35, 38.4],
-				backgroundColor: [
-					'rgba(255, 255, 255, 1)',
-					'rgba(255, 255, 255, 1)'
-				],
-				borderColor: [
-					'rgba(255, 255, 255, 1)',
-					'rgba(255, 255, 255, 1)'
-				],
-				borderWidth: 1
-			}]
-		},
-		options: {
-			responsive: true,
-			maintainAspectRatio: false,
-			scales: {
-				yAxes: [{
-					ticks: {
-						beginAtZero: false,
-						min: 30,
-						max: 40
-					}
-				}]
-			}
-		}
-	});
-	
-	var treePlantings = new Chart(ctx2, {
-		type: 'bar',
-		data: {
-			labels: [['No New', 'Trees'], ['New Trees', 'Within 50ft.']],
-			datasets: [{
-				label: 'Change to Avg. House',
-				data: [0, 3400],
-				backgroundColor: [
-					'rgba(255, 255, 255, 1)',
-					'rgba(255, 255, 255, 1)'
-				],
-				borderColor: [
-					'rgba(255, 255, 255, 1)',
-					'rgba(255, 255, 255, 1)'
-				],
-				borderWidth: 1
-			}]
-		},
-		options: {
-			responsive: true,
-			maintainAspectRatio: false,
-			scales: {
-				yAxes: [{
-					ticks: {
-						beginAtZero:true
-					}
-				}]
-			}
-		}
-		
-	});
-	
-	
-	
-	
-});
+//$(function(){
+//	//Vacant Lot Chart
+//	console.log(firing);
+//	Chart.defaults.global.defaultFontColor = '#fff';
+////	Chart.defaults.global.maintainAspectRatio = false;
+//	
+//	var ctx = $("#js-vacant-land-chart"),
+//		ctx2 = $('#js-tree-plantings-chart');
+//	
+//	var vacantLot = new Chart(ctx, {
+//		type: 'bar',
+//		data: {
+//			labels: [['No New', 'Trees'], ['New Trees', 'Within 50ft.']],
+//			datasets: [{
+//				label: 'House Price (Thousands)',
+//				data: [35, 38.4],
+//				backgroundColor: [
+//					'rgba(255, 255, 255, 1)',
+//					'rgba(255, 255, 255, 1)'
+//				],
+//				borderColor: [
+//					'rgba(255, 255, 255, 1)',
+//					'rgba(255, 255, 255, 1)'
+//				],
+//				borderWidth: 1
+//			}]
+//		},
+//		options: {
+//			responsive: true,
+//			maintainAspectRatio: false,
+//			scales: {
+//				yAxes: [{
+//					ticks: {
+//						beginAtZero: false,
+//						min: 30,
+//						max: 40
+//					}
+//				}]
+//			}
+//		}
+//	});
+//	
+//	var treePlantings = new Chart(ctx2, {
+//		type: 'bar',
+//		data: {
+//			labels: [['No New', 'Trees'], ['New Trees', 'Within 50ft.']],
+//			datasets: [{
+//				label: 'Change to Avg. House',
+//				data: [0, 3400],
+//				backgroundColor: [
+//					'rgba(255, 255, 255, 1)',
+//					'rgba(255, 255, 255, 1)'
+//				],
+//				borderColor: [
+//					'rgba(255, 255, 255, 1)',
+//					'rgba(255, 255, 255, 1)'
+//				],
+//				borderWidth: 1
+//			}]
+//		},
+//		options: {
+//			responsive: true,
+//			maintainAspectRatio: false,
+//			scales: {
+//				yAxes: [{
+//					ticks: {
+//						beginAtZero:true
+//					}
+//				}]
+//			}
+//		}
+//		
+//	});
+//	
+//	
+//	
+//	
+//});
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,27 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 var ctrlMain = require('../controllers/main');
-var ctrlAbout = require('../controllers/about');
-var ctrlPress = require('../controllers/press');
+//var ctrlAbout = require('../controllers/about');
+//var ctrlPress = require('../controllers/press');
 var ctrlForms = require('../controllers/forms');
-var ctrlJobs = require('../controllers/jobs')
+//var ctrlJobs = require('../controllers/jobs')
 
 /* Home Page */
 router.get('/', ctrlMain.index);
 
 /* About Pages */
-router.get('/about-us', ctrlAbout.about);
-router.get('/about-us/team', ctrlAbout.team);
-router.get('/about-us/projects', ctrlAbout.projects);
+//router.get('/about-us', ctrlAbout.about);
+//router.get('/about-us/team', ctrlAbout.team);
+//router.get('/about-us/projects', ctrlAbout.projects);
 
 /* Press Page */
-router.get('/press', ctrlPress.press);
+//router.get('/press', ctrlPress.press);
 
 /* Form Handling */
 router.post('/contact', ctrlForms.contactForm);
 router.get('/contact/thank-you', ctrlForms.formThankYou);
 
 /* Hidden */
-router.get('/jobs', ctrlJobs.jobs);
+//router.get('/jobs', ctrlJobs.jobs);
 
 module.exports = router;
